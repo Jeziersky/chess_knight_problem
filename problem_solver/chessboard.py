164755b0e98609f7854cc2ee1e6f27315b24c495
Jeziersky <b.jeziersky@gmail.com>
2019-08-01 21:05:12 +0200

    Refactor chessboard.py code

--- problem_solver/chessboard.py
+++ problem_solver/chessboard.py
@@ -1,30 +1,61 @@
+from collections import namedtuple
+
+Board = namedtuple('Board', 'min max')
+SIZE_OF_BOARD = Board(0, 4)
+
+
+class ChessFigure:
+    """ Generic chess figure """
+    __figure_moves = ()
+    name = None
+    board = None
+
+    def __init__(self, name, figure_moves, board):
+        self.name = name
+        self.__figure_moves = figure_moves
+        self.board = board
+
+    @property
+    def figure_moves(self):
+        return self.__figure_moves
+
+    def move(self, x, y):
+        raise NotImplementedError
+
+    def check_move(self, x, y):
+        raise NotImplementedError
+
+
+class KnightFigure(ChessFigure):
+    """ Class representing knight figure """
+    def __init__(self, board):
+        figure_moves = ((-2, 1), (-1, 2), (1, 2), (2, 1),
+             (2, -1), (1, -2), (-1, -2), (-2, -1))
+        self.counter = 1
+        super().__init__("Knight", figure_moves, board)
+
+    def move(self, x, y):
+        """ Make all posible moves for knight"""
+        if self.counter == 25:
+            return True
+        for jump in self.figure_moves:
+            after_x = x + jump[0]
+            after_y = y + jump[1]
+            if self.check_move(after_x, after_y):  # Move knight if true
+                self.counter += 1
+                self.board[after_x][after_y] = self.counter
+                self.move(after_x, after_y)
+
+    def check_move(self, x, y):
+        """ Check move correctness """
+        return SIZE_OF_BOARD.min <= x <= SIZE_OF_BOARD.max and SIZE_OF_BOARD.min <= y <= SIZE_OF_BOARD.max and self.board[x][y] == 0
+
+
 def make_board(start_x=0, start_y=0):
-    if 0 <= start_x <= 4 and 0 <= start_y <= 4:
-        board = []
-        for i in range(0, 5):
-            board.append(5 * [0])
+    """ Create game board """
+    if SIZE_OF_BOARD.min <= start_x <= SIZE_OF_BOARD.max and SIZE_OF_BOARD.min <= start_y <= SIZE_OF_BOARD.max:
+        board = [5*[0] for i in range(SIZE_OF_BOARD.min, SIZE_OF_BOARD.max+1)]
         board[start_x][start_y] = 1
-        counter = 1
-        return counter, board
+        return board
     else:
         return ValueError
-
-
-def check_move(x, y, board):
-    """ Check move correctness """
-    return 0 <= x <= 4 and 0 <= y <= 4 and board[x][y] == 0
-
-
-def move(x, y, counter, board):
-    """ Make all posible moves for knight"""
-    if counter == 25:
-        return True
-    jumps = ((-2, 1), (-1, 2), (1, 2), (2, 1),
-             (2, -1), (1, -2), (-1, -2), (-2, -1))  # All knight moves
-    for jump in jumps:
-        after_x = x + jump[0]
-        after_y = y + jump[1]
-        if check_move(after_x, after_y, board):  # If true knight
-            counter += 1
-            board[after_x][after_y] = counter
-            move(after_x, after_y, counter, board)
--- problem_solver/views.py
+++ problem_solver/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render
 from django.views import View
 
-from problem_solver.chessboard import move, make_board
+from problem_solver.chessboard import KnightFigure, make_board, SIZE_OF_BOARD
 from problem_solver.models import Knight, Board, Field
 
 
 class LandingPageView(View):
     def get(self, request):
         """ Main page of app """
-        board_range = range(5)
+        board_range = range(SIZE_OF_BOARD.max+1)
         return render(request, 'app/main.html', context={'range': board_range})
 
     def post(self, request):
         """ POST request handler """
         place = tuple(request.POST.get('place'))
         knight = Knight.objects.create(position_x=int(place[0]), position_y=int(place[1]))
-        start_x = knight.position_x
-        start_y = knight.position_y
-        counter, board = make_board(start_x, start_y)
-        move(start_x, start_y, counter, board)
+        start_x, start_y = knight.position_x, knight.position_y
+        board = make_board(start_x, start_y)
+        knight_figure = KnightFigure(board)
+        knight_figure.move(start_x, start_y)
         obj_board = Board.objects.create()
-        for i in range(5):
-            for j in range(5):
+        for i in range(SIZE_OF_BOARD.max+1):
+            for j in range(SIZE_OF_BOARD.max+1):
                 value = board[i][j]
                 Field.objects.create(board=obj_board, position_x=i, position_y=j, value=value)
         return render(request, 'app/table.html', context={'board': board})
